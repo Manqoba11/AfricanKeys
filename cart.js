e6dc8551a9b964bee42ba06854110e71c588e3a7
Manqoba11 <manqobamk12@gmail.com>
2026-06-21 13:00:16 +0200

    update

--- cart.js
+++ cart.js
@@ -365,3 +365,36 @@ window.onload = function () {
     const lb = document.getElementById("logoutBtn");
     if (lb) lb.addEventListener("click", handleLogout);
 };
+
+
+
+function toggleMenu() {
+    document.getElementById("navMenu").classList.toggle("active");
+}
+document.addEventListener("DOMContentLoaded", () => {
+
+    const loggedIn =
+    localStorage.getItem("loggedIn");
+
+    const loginLink =
+    document.getElementById("loginLink");
+
+    const signupLink =
+    document.getElementById("signupLink");
+
+    const logoutLink =
+    document.getElementById("logoutLink");
+
+    if(loggedIn === "true"){
+
+        if(loginLink)
+            loginLink.style.display = "none";
+
+        if(signupLink)
+            signupLink.style.display = "none";
+
+        if(logoutLink)
+            logoutLink.style.display = "block";
+
+    }
+});
