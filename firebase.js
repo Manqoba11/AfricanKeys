e2cf8340cd60179234d9c6a5f3da759c7aceb4ad
Manqoba11 <manqobamk12@gmail.com>
2026-07-12 15:23:45 +0200

    m

--- firebase.js
+++ firebase.js
@@ -8,9 +8,9 @@ const firebaseConfig = {
     storageBucket: "keys-of-africa.firebasestorage.app",
     messagingSenderId: "295412124500",
     appId: "1:295412124500:web:db00efdbe5319869b416c1"
-};
+}; 
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { auth }; 
+export { auth };   
