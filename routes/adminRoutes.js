b6da4eb85f912d1fddc0d8ae172f6f0e62bf9b0c
Manqoba11 <manqobamk12@gmail.com>
2026-07-20 20:45:49 +0200

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,6 +4,12 @@ const router = express.Router();
 
 const adminController = require("../controllers/adminController");
 
-router.get("/dashboard", adminController.dashboard);
+router.get("/dashboard", (req, res) => {
+
+    console.log("Dashboard route hit");
+
+    adminController.dashboard(req, res);
+
+});
 
 module.exports = router;
