49798339c08665f6cc805ebfb4eebf83588f01b7
Manqoba11 <manqobamk12@gmail.com>
2026-07-20 19:27:07 +0200

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,21 +1,12 @@
 const express = require("express");
-
-console.log("✅ productRoutes.js loaded");
-
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    console.log("GET /api/products called");
-    res.json({
-        message: "Products route is working!"
-    });
-});
+const productController = require("../controllers/productController");
 
-router.get("/:id", (req, res) => {
-    console.log("GET /api/products/:id called");
-    res.json({
-        id: req.params.id
-    });
-});
+// Get all products
+router.get("/", productController.getProducts);
+
+// Get one product
+router.get("/:id", productController.getProduct);
 
 module.exports = router;
