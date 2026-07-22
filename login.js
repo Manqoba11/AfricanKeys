06cd0b7aeca311e9530fb1a96f3f2de3f17c0c51
Manqoba11 <manqobamk12@gmail.com>
2026-07-22 08:47:20 +0200

    Update login.js

--- login.js
+++ login.js
@@ -17,7 +17,7 @@ form.addEventListener("submit", async function (e) {
 
     try {
 
-        const response = await fetch("http://localhost:3000/api/auth/login", {
+        const response = await fetch("http://localhost:4000/api/auth/login", {
 
             method: "POST",
 
